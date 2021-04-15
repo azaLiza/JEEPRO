083b90c5c415b6cae4812c17eef869e67e73c127
stevehunn <33178230+stevehunn@users.noreply.github.com>
2021-04-15 18:46:26 +0200

    ajout redirection

--- TodaysAdventure/src/Post.java
+++ TodaysAdventure/src/Post.java
@@ -39,22 +39,20 @@ public class Post implements Serializable {
         this.post = post;
     }
 
-    public boolean addPost() throws SQLException {
+    public String addPost() throws SQLException {
         PreparedStatement query = DBConnection.getInstance().prepareStatement("INSERT INTO posts (`id_user`, `content_post`, `date_post`) VALUES (?,?,?);");
         query.setString(1, title);
         query.setString(2, desc);
         query.setString(3, post);
-        return query.execute();
+        return query.execute() ? "home.xhtml" : "addPost.xhtml";
     }
 
-    public boolean getAPost() throws SQLException {
+    public void getAPost() throws SQLException {
 
         String username = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
         PreparedStatement query = DBConnection.getInstance().prepareStatement("SELECT content_post from posts INNER JOIN users u on posts.id_user = u.id_user where u.name like ? ORDER BY date_post;");
         query.setString(1, username);
         ResultSet result = query.executeQuery();
-        return true;
-
     }
 
 
