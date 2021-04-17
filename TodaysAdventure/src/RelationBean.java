fb04b0acbc1bb50282f63d65d44a02228f5a1756
Liza <summerlove3@hotmail.fr>
2021-04-17 13:10:39 +0200

    doesnt'work

--- TodaysAdventure/src/RelationBean.java
+++ TodaysAdventure/src/RelationBean.java
@@ -1,4 +1,8 @@
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
@@ -6,7 +10,10 @@ import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import databaseconnector.DBConnection;
 import tables.User;
+import DAO.DAORelation;
+import DAO.UseRelation;
 
 
 /**
@@ -27,9 +34,24 @@ public class RelationBean implements Serializable {
 	private List<User> myfriends;
 	
 	public List<User> getThem(){
-		HttpServletRequest request = null;
+		int id_session = 0;
+		/*HttpServletRequest request = null;
 		HttpSession session = request.getSession(true);
-		int id_user = (Integer) session.getAttribute("id_user");
+		int id_user = (Integer) session.getAttribute("id_user");*/
+		
+		String catchsess="select * from session";
+		Connection con=DBConnection.getInstance();
+		PreparedStatement pre=null;
+		try {
+			pre=con.prepareStatement(catchsess);
+			ResultSet resu=pre.executeQuery();
+			id_session=resu.getInt("id_session");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		DAORelation daorel=new UseRelation();
+		myfriends=daorel.getAllFriends(id_session);
 		return myfriends;
 	}
 
